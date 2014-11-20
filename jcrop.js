be11d622520ec803cb822ab15930f293f079afdc
ForeverGlory <foreverglory@qq.com>
2014-11-20 16:38:59 +0800

    更新JS Hook 的方式，使用 图片绑定事件（load,select,change,dbclick,release）

--- jcrop.js
+++ jcrop.js
@@ -1,64 +1,33 @@
-;
-Drupal.jcrop = Drupal.jcrop || {};
 (function ($) {
-    Drupal.jcrop.jcrop = {
-        change: function (box, coords) {
-            jcrop_update(box, coords);
-        },
-        select: function (box, coords) {
-            jcrop_update(box, coords);
-        },
-        dblclick: function (box, coords) {
-
-        },
-        release: function (box, coords) {
-
-        }
-    };
-    function jcrop_update(box, coords) {
-        $(".jcrop-x", box).val(coords.x);
-        $(".jcrop-y", box).val(coords.y);
-        $(".jcrop-w", box).val(coords.w);
-        $(".jcrop-h", box).val(coords.h);
-    }
     Drupal.behaviors.jcrop = {
         attach: function (context, settings) {
-            $('.jcrop-box', context).each(function () {
-                var $box = $(this);
-                $(".jcrop-image", $box).one("load", function () {
-                    var options = $.extend(settings.jcrop[$box.attr("id")] || {}, {
-                        onChange: function (c) {
-                            $.each(Drupal.jcrop, function () {
-                                if ($.isFunction(this.change)) {
-                                    this.change($box, c);
-                                }
-                            });
-                        },
-                        onSelect: function (c) {
-                            $.each(Drupal.jcrop, function () {
-                                if ($.isFunction(this.select)) {
-                                    this.select($box, c);
-                                }
-                            });
-                        },
-                        onDblClick: function (c) {
-                            $.each(Drupal.jcrop, function () {
-                                if ($.isFunction(this.dblclick)) {
-                                    this.dblclick($box, c);
-                                }
-                            });
-                        },
-                        onRelease: function (c) {
-                            $.each(Drupal.jcrop, function () {
-                                if ($.isFunction(this.release)) {
-                                    this.release($box, c);
-                                }
-                            });
-                        }
-                    });
-                    $(this).Jcrop(options);
-                }).trigger("load");
-            });
+            $('.jcrop-box .jcrop-image:not(.jcrop-processed)', context).one("jcrop", function (e) {
+                var $image = $(this);
+                var jcrop = null;
+                $image.Jcrop($.extend(settings.jcrop[$image.data("jcrop")] || {}, {
+                    onChange: function (coords) {
+                        $image.trigger("change", [coords, jcrop]);
+                    },
+                    onSelect: function (coords) {
+                        $image.trigger("select", [coords, jcrop]);
+                    },
+                    onDblClick: function (coords) {
+                        $image.trigger("dblclick", [coords, jcrop]);
+                    },
+                    onRelease: function (coords) {
+                        $image.trigger("release", [coords, jcrop]);
+                    }
+                }), function () {
+                    jcrop = this;
+                    $image.trigger("load", [null, jcrop]);
+                });
+            }).on("change select", function (e, c, j) {
+                var $image = $(this);
+                $image.siblings(".jcrop-x").val(c.x);
+                $image.siblings(".jcrop-y").val(c.y);
+                $image.siblings(".jcrop-w").val(c.w);
+                $image.siblings(".jcrop-h").val(c.h);
+            }).trigger("jcrop").addClass("jcrop-processed");
         }
     };
 })(jQuery);
